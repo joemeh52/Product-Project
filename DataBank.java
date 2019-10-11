e416fcf4a3e4c3ea3ea10a7fd020b5cec0e73866
Joe Hernandez <joehernandez@Joes-MBP-2.attlocal.net>
2019-10-10 19:24:21 -0500

    Second commit

--- DataBank.java
+++ DataBank.java
@@ -1,33 +1,39 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.*;
 public class DataBank{
   
 
 
-static Scanner sc = new Scanner(System.in);
+public static Scanner sc = new Scanner(System.in).useDelimiter("\\n");
 
 
+
+public static Product addItem() {
+	
+	   System.out.println("Enter Name: ");
+	   String n = sc.next();
+	   System.out.println("Enter Sku: ");
+	   int sn = sc.nextInt();
+	   System.out.println("Enter Description: ");
+	   String d = sc.next();
+	   System.out.println("Enter Aisle: ");
+	   String l = sc.next();
+
+	   return new Product(n, sn, d, l);
+	
+}
 
 
 
   public static void main(String[] args){
-	  ArrayList<Product> pog = new ArrayList<Product>();
+	 ArrayList<Product> pog = new ArrayList<Product>();
    System.out.println("This will help with product placement!");
   // Product windex = new Product("Windex", 7777777, "Cleaning liquid", "Aisle 265 Sec M");
   // System.out.println(windex);
   
-
-   System.out.println("Enter Name: ");
-   String n = sc.next();
-   System.out.println("Enter Sku: ");
-   int sn = sc.nextInt();
-   System.out.println("Enter Description: ");
-   String d = sc.next();
-   System.out.println("Enter Aisle: ");
-   String l = sc.next();
-
-   pog.add(new Product(n, sn, d, l));
-   
+   	pog.add(addItem());
+  
 	System.out.println(pog);
 
 
