f5b28a7061511524f976ba05463df563d88756c3
Joe Hernandez <joehernandez@Joes-MBP-2.attlocal.net>
2019-10-13 17:08:42 -0500

    Third Commit

--- DataBank.java
+++ DataBank.java
@@ -6,12 +6,14 @@ public class DataBank{
 
 
 public static Scanner sc = new Scanner(System.in).useDelimiter("\\n");
+public static ArrayList<Product> pog = new ArrayList<Product>();
 
 
-
-public static Product addItem() {
+public static void addItem() {
+	String addAnother;
 	
-	   System.out.println("Enter Name: ");
+	  do {
+		System.out.println("Enter Name: ");
 	   String n = sc.next();
 	   System.out.println("Enter Sku: ");
 	   int sn = sc.nextInt();
@@ -20,19 +22,52 @@ public static Product addItem() {
 	   System.out.println("Enter Aisle: ");
 	   String l = sc.next();
 
-	   return new Product(n, sn, d, l);
+	  
+	   pog.add(new Product(n,sn,d,l));
+	   
+	   
+	   System.out.println("\nWould you like to add another? (Y/N)");
+	   addAnother = sc.next();
+	   
+	  }while (!"n".equals(addAnother));
+}
+
+
+public static void menu(){
+	System.out.println("Welcome to the Systems Main Menu!");
+	System.out.println("What would you like to do?");
+	System.out.println("1) Add items \n" + "2) Coming Soon!!!" );
+	int decision = sc.nextInt();
+	switch(decision)
+	{
+	case 1: 
+		addItem();
+		break;
+	case 2:
+		System.out.println("User option coming soon!!!");
+		menu();
+		break;
+	default:
+		System.out.println("\nChoose a valid option.");
+		menu();
+		break;
+	}
+	
+	
+	
+	
 	
 }
 
 
-
   public static void main(String[] args){
-	 ArrayList<Product> pog = new ArrayList<Product>();
+	  //ArrayList<Product> pog = new ArrayList<Product>();
    System.out.println("This will help with product placement!");
   // Product windex = new Product("Windex", 7777777, "Cleaning liquid", "Aisle 265 Sec M");
   // System.out.println(windex);
   
-   	pog.add(addItem());
+   	menu();
+   //pog.add(addItem());
   
 	System.out.println(pog);
 
